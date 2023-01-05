d54a2325df7c45f7023cb76828339d4f6a959307
Sina <alisinai850@gmail.com>
2023-01-05 12:41:39 +0330

    test

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ void Field (char field[50][25]){
     for (int i = 0; i < 50; i++){
         for (int j = 0; j < 25; j++){
             if (i==0 || i==49 || j==0 || j==24){
-                field[i][j] = '#';
+                field[i][j] = '$';
             }
         }
     }
