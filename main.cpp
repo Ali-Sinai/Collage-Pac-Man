18e33ca650ebc1788f4d9524ebbfc46157376e12
Sina <alisinai850@gmail.com>
2023-01-05 12:49:25 +0330

    created initfield and printfield funcs

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void Field (char field[50][25]){
-    for (int i = 0; i < 50; i++){
-        for (int j = 0; j < 25; j++){
-            if (i==0 || i==49 || j==0 || j==24){
-                field[i][j] = '$';
+void InitField (char field[25][50]){
+    for (int i = 0; i < 25; i++){
+        for (int j = 0; j < 50; j++){
+            if (i==0 || i==24 || j==0 || j==49){
+                field[i][j] = '#';
+            }else{
+                field[i][j] = ' ';
             }
         }
     }
 }
 
+void PrintField(char field[25][50]){
+    for (int i = 0; i < 25; i++){
+        for (int j = 0; j < 50; j++){
+            cout<<field[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
-    // i have no fucking idea.
+    char field[25][50];
+    InitField(field);
+    PrintField(field);
 }
