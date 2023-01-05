9c29fe06c5c9ed7ba47206873aed11a8e9ed0058
Sina <alisinai850@gmail.com>
2023-01-05 13:51:18 +0330

    This is Version 1.0, it works fine fwith w s d a keys; I didn;t do the blocks yet

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 using namespace std;
 
-int i=0, j=0;
+int i=1, j=1;
 
 void InitField (char field[25][50]){
     for (int i = 0; i < 25; i++){
@@ -15,7 +15,6 @@ void InitField (char field[25][50]){
             }
         }
     }
-    field[1][1] = '>';
 }
 
 void PrintField(char field[25][50]){
@@ -31,16 +30,24 @@ void GetKey(int &x, int &y){
     char k = getch();
     switch (k){
         case 'w':
-            x -= 1;
+            if (x-1 > 0 && x-1<24){
+                x -= 1;
+            };
             break;
         case 's':
-            x += 1;
+            if (x+1 > 0 && x+1<24){
+                x += 1;
+            };
             break;
         case 'a':
-            y -= 1;
+            if (y-1 > 0 && y-1<49){
+                y -= 1;
+            }
             break;
         case 'd':
-            y += 1;
+            if (y+1 > 0 && y+1<49){
+                y += 1;
+            }
             break;
     }
 
@@ -48,12 +55,11 @@ void GetKey(int &x, int &y){
 
 void OrdinaryMove(char Field[25][50]){
     GetKey(i,j);
-    if (i>1 && j>1 && i<24 && j<49){
-        for (int t = 1; t < 24; t++){
-            for(int k = 1; k < 49; k++){
-                if ( t == i && k == j){
-                    Field[t][k] = '>';
-                }
+    InitField(Field);
+    for (int t = 1; t < 24; t++){
+        for(int k = 1; k < 49; k++){
+            if ( t == i && k == j){
+                Field[t][k] = '>';
             }
         }
     }
@@ -65,6 +71,7 @@ void OrdinaryMove(char Field[25][50]){
 int main(){
     char field[25][50];
     InitField(field);
+    field[1][1] = '>';
     PrintField(field);
     OrdinaryMove(field);
 }
